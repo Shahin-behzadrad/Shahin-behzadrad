bc51fccdd492770c6aa4d4e114f8947d119aaabe
shahin behzadrad <shahinbzr2267@gmail.com>
2025-01-17 20:49:23 +0330

    feat: implementing Hero and Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import classes from "./Header.module.scss";
+
+import logo1 from "@/assets/logo/logo-1.png";
+import logo2 from "@/assets/logo/logo-2.png";
+
+const Header = () => {
+  return (
+    <nav className={classes.header}>
+      <div className={classes.logoContainer}>
+        <Image src={logo1} width={20} alt="logo" />
+        <Image src={logo2} width={20} alt="logo" />
+      </div>
+      <div className={classes.navbarBtns}>
+        <button>About</button>
+        <button>Experiences</button>
+        <button>Projects</button>
+        <button>Contact</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Text from "../shared/Text";
+import classes from "./Hero.module.scss";
+
+import logo1 from "@/assets/logo/logo-1.png";
+import logo2 from "@/assets/logo/logo-2.png";
+
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.shape1} />
+      <div className={classes.shape2} />
+      <div className={classes.shape3} />
+
+      <div className={classes.glassyLayer} />
+
+      <div className={classes.hero}>
+        <div className={classes.heroContent}>
+          <Text
+            value="Shahin Behzadrad /"
+            fontSize="xxxxl"
+            htmlTag="h1"
+            textAlign="center"
+            startAdornment={<Image src={logo1} width={50} alt="logo1" />}
+            endAdornment={<Image src={logo2} width={50} alt="logo1" />}
+          />
+          <div className={classes.roleContent}>
+            <div>
+              <Text
+                value="Front-End Developer"
+                fontSize="xl"
+                htmlTag="h2"
+                textAlign="left"
+              />
+              <Text
+                value="(with Full-Stack Capabilities)"
+                color="secondary"
+                textAlign="left"
+                fontSize="xxl"
+              />
+            </div>
+            <Text
+              className={classes.roleDescription}
+              value="Front-End Developer skilled in creating clean and optimized user interfaces, with additional experience in back-end technologies like Node.js, MySQL, and Sequelize."
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
